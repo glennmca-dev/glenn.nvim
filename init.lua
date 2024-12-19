d08b54cb9e73857752d461f8c5174f445bdbabc5
gmcallister-r7 <85949578+gmcallister-r7@users.noreply.github.com>
2024-12-19 14:06:43 +0000

    improve cmp conf

--- init.lua
+++ init.lua
@@ -793,6 +793,24 @@ require('lazy').setup({
           end,
         },
         completion = { completeopt = 'menu,menuone,noinsert' },
+        window = {
+          completion = {
+            winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
+            col_offset = -3,
+            side_padding = 0,
+          },
+        },
+        formatting = {
+          fields = { 'kind', 'abbr', 'menu' },
+          format = function(entry, vim_item)
+            local kind = require('lspkind').cmp_format { mode = 'symbol_text', maxwidth = 50 }(entry, vim_item)
+            local strings = vim.split(kind.kind, '%s', { trimempty = true })
+            kind.kind = ' ' .. (strings[1] or '') .. ' '
+            kind.menu = '    (' .. (strings[2] or '') .. ')'
+
+            return kind
+          end,
+        },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
@@ -859,6 +877,7 @@ require('lazy').setup({
       }
     end,
   },
+  { 'onsails/lspkind.nvim' },
   {
     'catppuccin/nvim',
     name = 'catppuccin',
